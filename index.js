c14a4a24e49267938698bdb80bea477284f47272
Subhodip Roy <subhodiproy281@gmail.com>
2023-10-29 11:45:36 +0530

    configured cors

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const voiceID = "kgG7dCoKCfLehAPWkJOE";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 const port = 3000;
 
 app.get("/", (req, res) => {
